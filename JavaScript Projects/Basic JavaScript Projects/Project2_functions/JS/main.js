30c2dd78a35f463b5bb287580427e89fd9ac1aaf
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2020-11-18 17:45:08 -0800

    Add files via upload

---
+++ JavaScript Projects/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+function buttonFunction() {       
+    var str = "This is brown";
+    var result = str.fontcolor("brown"); 
+    document.getElementById("Blue_Text") .innerHTML = result; 
+}
+
+function function3() {
+    var sentence = "This course is tough";
+    sentence += " but we\'re still on course!";
+    document.getElementById("text1") .innerHTML = sentence;
+}
+
+
+    document.getElementById("Blue_Text");
