60cb02640979dba6f3857add697d29d157d600f9
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2020-11-20 17:40:09 -0800

    Add files via upload

--- JavaScript Projects/Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
-function my_Dictionary() {
+function my_Dictionary() {    // setting up a kvp dictionary
     var vehicle = {
-        make: "Ford",
+        make: "Ford",         // different key values within the dictionary
         model:"Bronco",
         year:"1993",
         owner:"OJ Simpson"   
      };
-     delete vehicle.make;
+     delete vehicle.make;    // deleting the make from the dictionary
      document.getElementById("Dictionary").innerHTML=vehicle.owner;
 
      document.getElementById("Dictionary2").innerHTML=vehicle.make;
