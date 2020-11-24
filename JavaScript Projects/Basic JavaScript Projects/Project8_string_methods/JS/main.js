1e14e51960561cf0b13ec0fd0e476a207ff43bab
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2020-11-23 16:16:28 -0800

    Add files via upload

--- JavaScript Projects/Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
-function full_Sentence() {
+function full_Sentence() { //concat function sentence structure and layout
     var part_1 = "I am going";
     var part_2 = " to keep writing";
     var part_3 = " until this sentence";
@@ -8,25 +8,25 @@ function full_Sentence() {
 }
 
 
-function slice_Method() {
+function slice_Method() {  //this slice method identifies and executes the section variable specifiations on the line of the first var
     var Sentence = "All work and no play makes Johnny a dull boy.";
     var Section = Sentence.slice(27,33);
     document.getElementById("slice").innerHTML = Section;
 }
 
-var str = "Hello World!";
+var str = "Hello World!";   //all uppercase challenge function
 var res = str.toUpperCase();
 document.write(res);
 
-var str = "Mr. Blue has a blue house";
+var str = "Mr. Blue has a blue house"; //search function challenge
 var n = str.search("blue");
 
-function string_Method() {
+function string_Method() {  //string method function
     var X =182;
     document.getElementById("numberstoString").innerHTML = X.toString();
 }
 
-function precision_Method() {
+function precision_Method() { // precision function that cuts down the variable number into 5 digits
     var X = 12938.3012987376112;
     document.getElementById("Precision").innerHTML = X.toPrecision(5);
 }
